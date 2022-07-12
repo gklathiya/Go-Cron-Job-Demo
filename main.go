280f3155acd7be9e5d8415ad324a3cfa7c5b18fe
G K Lathiya <gopal.lathiya@gmail.com>
2022-07-12 19:07:21 +0530

    done cron in Go

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ func Job(name string) {
 
 func runCronJob() {
 	schedule := cron.New()
-	schedule.AddFunc("*/1 * * * *", func() {
+	schedule.AddFunc("*/1 * * * *", func() { // This will run job in every 1 minutes interval
 		Job("Hello Philia, Shree Calling you...")
 	})
 	log.Info("Start cron after every minute")
 	schedule.Start()
-	time.Sleep(4 * time.Minute)
+	time.Sleep(4 * time.Minute) // wait 4 minutes to complere this task.
 }
 
 func main() {
